85b390188a73e9d8ec6c85b61f2eb37cb51a1a6d
leen <leen03255@gmail.com>
2023-12-30 21:55:18 +0300

    edits

--- lib/presentation/time_management.dart
+++ lib/presentation/time_management.dart
@@ -4,16 +4,19 @@ import 'package:uni_verse/presentation/time_management_tabs/to_do.dart';
 
 class TimeManagement extends StatelessWidget {
   static String route = 'TimeManagement';
+
   const TimeManagement({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
+          backgroundColor: Color(0xFF371942),
           title: const Text('Manage your time'),
           bottom: const TabBar(tabs: [
-            Tab(icon: Icon(Icons.calendar_month)),
+            Tab(icon: Icon(Icons.calendar_month, color: Color(0xFF371942),)),
             Tab(
                 child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -22,10 +25,7 @@ class TimeManagement extends StatelessWidget {
           ]),
         ),
         body: const TabBarView(
-          children: [
-            Calendar(),
-            ToDo()
-          ],
+          children: [Calendar(), ToDo()],
         ),
       ),
     );
